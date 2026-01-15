3c9f77e0e857865c102f60e1fc13396195cd29ca
Roei Katabi <roeikatabi@gmail.com>
2026-01-15 11:19:00 +0200

    First commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+import authRoutes from "./routes/auth.routes.js";
+import appRoutes from "./routes/app.routes.js";
+import sdkRoutes from "./routes/sdk.routes.js";
+import dashboardRoutes from "./routes/dashboard.routes.js";
+import errorHandler from "./middleware/errorHandler.js";
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+app.use("/apps", appRoutes);
+app.use("/sdk", sdkRoutes);
+app.use("/dashboard", dashboardRoutes);
+
+app.use(errorHandler);
+
+export default app;
